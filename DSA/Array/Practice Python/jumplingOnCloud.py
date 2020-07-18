e18609498ce4b2f13a926f56d915c834664c8644
gk-svg <gk32239@gmail.com>
2020-07-18 21:27:24 +0530

    problem solving

--- DSA/Array/Practice Python/jumplingOnCloud.py
+++ DSA/Array/Practice Python/jumplingOnCloud.py
@@ -1,20 +1,9 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-# Complete the jumpingOnClouds function below.
 def jumpingOnClouds(c):
     count=[]
     i=0
     c.append(1)
     c.append(1)
     while(i<(len(c)-2)):
-        # print(i+1,end='')
-        # print(' ', i+2,end='')
         if c[i+2]!=1 and i+2 not in count:
             count.append(i+2)
             i+=2
@@ -26,17 +15,9 @@ def jumpingOnClouds(c):
     return len(count)
 
 
-
-
 if __name__ == '__main__':
-    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input())
-
     c = list(map(int, input().rstrip().split()))
-
     result = jumpingOnClouds(c)
     print(result)
-    # fptr.write(str(result) + '\n')
-
-    # fptr.close()
+    
