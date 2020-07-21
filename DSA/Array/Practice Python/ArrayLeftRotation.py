3a53229b1318331ba3211e97c22913ed6b54c237
gk-svg <gk32239@gmail.com>
2020-07-21 23:59:56 +0530

    Practice Problems

---
+++ DSA/Array/Practice Python/ArrayLeftRotation.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the rotLeft function below.
+# def rotLeft(a, d):
+#     for i in range(d):
+#         temp=a[0]
+#         for i in range(1,len(a)):
+#             a[i-1]=a[i]
+#         a[len(a)-1]=temp
+#     return a
+def rotLeft(a, d):
+    for i in range(len(a)):
+        t=i-d
+        if t<0:
+            t*=(-1)
+        a[i]=a[t]
+    return a
+
+
+if __name__ == '__main__':
+    
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    print(result)
