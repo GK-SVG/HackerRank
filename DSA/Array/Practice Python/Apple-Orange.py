360ef209af3d5fc7f2608c8f4246892154f4bc50
gk-svg <gk32239@gmail.com>
2020-07-28 19:48:43 +0530

    practice problem

---
+++ DSA/Array/Practice Python/Apple-Orange.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countApplesAndOranges function below.
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    apple=0
+    orange=0
+    for i in range(len(apples)):
+        if apples[i]+a>=s and apples[i]+a<=t:
+            apple+=1
+    for i in range(len(oranges)):
+        if oranges[i]+a>=s and oranges[i]+a<=t:
+            orange+=1
+    print(apple)
+    print(orange)
+
+
+
+if __name__ == '__main__':
+    st = input().split()
+
+    s = int(st[0])
+
+    t = int(st[1])
+
+    ab = input().split()
+
+    a = int(ab[0])
+
+    b = int(ab[1])
+
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
